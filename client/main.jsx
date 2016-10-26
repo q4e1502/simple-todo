7a87f3a1c98677b801fd15e0798f41449ad653d1
qle@stacck.com <qle@stacck.com>
2016-10-26 15:11:36 +0700

    Add component google maps

--- client/main.jsx
+++ client/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 
 import App from '../imports/ui/client/App.jsx';
+import Maps from '../imports/ui/client/Map.jsx';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 Meteor.startup(() => {
 	injectTapEventPlugin();
-  render(<App />, document.getElementById('render-target'));
+  render((
+  	<Router history={hashHistory}>
+	    <Route path="/" component={App}/>
+	    <Route path="/map" component={Maps}/>
+	  </Router>
+	), document.getElementById('render-target'));
 });
---
+++ imports/ui/client/Map.jsx
@@ -0,0 +1,12 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+const apiKey = 'AIzaSyC0H1qlWHMU6LYQwceqv5oggIQlpBbCmIU';
+
+export default class Maps extends Component {
+	render() {
+		return (
+			<div>123</div>
+		)
+	}
+}
